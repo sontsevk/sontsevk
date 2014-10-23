d5ad13937da56ca1fa85ad6f3df8fe4d227f25cf
chabanenk0 <chabanenk0@github.com>
2014-10-24 01:07:07 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="windows-1251">
+<title>Сонце в кишені</title>
+<style>
+body,h1,h3 {text-align: center}
+</style>
+</head>
+<body>
+<h1>Сонце в кишені</h1>
+<a href="wordpress"><img src="https://pp.vk.me/c616119/v616119129/539d/38HwQRGynSg.jpg" width="40%" title = "Enter the main site"></a>
+<h3>Ми в соціальних мережах:</h3>
+<span><a href="https://vk.com/sontsevk"><img src="vk.png" width="64px"></a> </span>
+<span><a href="https://twitter.com/SontseVK"><img src="tw.png" width="64px"></a> </span>
+<span><a href="https://www.youtube.com/user/sontsevk"><img src="yt.png" width="64px"></a> </span>
+<span><a href="https://www.facebook.com/sontsevk"><img src="fb.png" width="64px"></a> </span>
+
+</body>
+</html>
---
+++ wordpress/wp-content/themes/san-kloud/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package San Kloud
+ */
+?>
+<div style="text-align:center;">
+<div>Ми в соціальних мережах:</div>
+<span><a href="https://vk.com/sontsevk"><img src="/vk.png" width="32px"></a> </span>
+<span><a href="https://twitter.com/SontseVK"><img src="/tw.png" width="32px"></a> </span>
+<span><a href="https://www.youtube.com/user/sontsevk"><img src="/yt.png" width="32px"></a> </span>
+<span><a href="https://www.facebook.com/sontsevk"><img src="/fb.png" width="32px"></a> </span>
+</div>
+<div class="container-12 footer">
+	<a href="http://wordpress.org/" rel="generator">Proudly powered by WordPress</a> |
+<?php printf( __( 'Theme: %1$s by %2$s.', 'san-kloud' ), 'San Kloud', '<a href="http://kovshenin.com/" rel="designer">Konstantin Kovshenin</a>' ); ?>
+	<br class="clear" />
+</div>
+</div>
+<?php wp_footer();?>
+</body>
+</html>
