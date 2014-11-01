88fb4515e2afca7cd96177020a818ebc6358bedf
Dmitry Chabanenko <chabanenk0>
2014-11-01 15:44:40 +0200

    fixed title page

--- index.php
+++ index.php
@@ -8,13 +8,12 @@ body,h1,h3 {text-align: center}
 </style>
 </head>
 <body>
-<h1>Сонце в кишені</h1>
-<a href="wordpress"><img src="https://pp.vk.me/c616119/v616119129/539d/38HwQRGynSg.jpg" width="40%" title = "Enter the main site"></a>
-<h3>Ми в соціальних мережах:</h3>
+<a href="wordpress"><img src="https://pp.vk.me/c616119/v616119129/539d/38HwQRGynSg.jpg" width="35%" title = "Enter the main site"></a>
+<div>
 <span><a href="https://vk.com/sontsevk"><img src="vk.png" width="64px"></a> </span>
 <span><a href="https://twitter.com/SontseVK"><img src="tw.png" width="64px"></a> </span>
 <span><a href="https://www.youtube.com/user/sontsevk"><img src="yt.png" width="64px"></a> </span>
 <span><a href="https://www.facebook.com/sontsevk"><img src="fb.png" width="64px"></a> </span>
-
+</div>
 </body>
 </html>
